33ebd2931081bbbb0674ff3c2628c8103a3d1532
aniket-bhadra <aniketbhadra98320@gmail.com>
2024-01-04 16:29:24 +0530

    new options for the toolbar added

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -2,6 +2,18 @@ import React, { useCallback } from "react";
 import Quill from "quill";
 import "quill/dist/quill.snow.css";
 
+const TOOLBAR_OPTIONS = [
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ font: [] }],
+  [{ list: "ordered" }, { list: "bullet" }],
+  ["bold", "italic", "underline"],
+  [{ color: [] }, { background: [] }],
+  [{ script: "sub" }, { script: "super" }],
+  [{ align: [] }],
+  ["image", "blockquote", "code-block"],
+  ["clean"],
+];
+
 const TextEditor = () => {
   const wrapperRef = useCallback((wrapper) => {
     if (wrapper == null) return;
@@ -13,6 +25,9 @@ const TextEditor = () => {
     wrapper.append(editor);
     new Quill(editor, {
       theme: "snow",
+      modules: {
+        toolbar: TOOLBAR_OPTIONS,
+      },
     });
   }, []);
   return <div className="container" ref={wrapperRef}></div>;
