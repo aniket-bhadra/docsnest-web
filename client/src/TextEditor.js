18e8cfd489b6208d98e7951a1a3a75e82ef1bce2
aniket-bhadra <aniketbhadra98320@gmail.com>
2025-03-03 09:32:02 +0530

    responsive styles issue : fixed

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -162,6 +162,24 @@ const TextEditor = () => {
           </div>
           <div className="document-status">
             <span className="save-status">Saved</span>
+
+            <button
+              className="share-button modal-you"
+              onClick={() =>
+                (document.getElementById("shareModal").style.display = "flex")
+              }
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                viewBox="0 0 16 16"
+              >
+                <path d="M13.5 1a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zM11 2.5a2.5 2.5 0 1 1 .603 1.628l-6.718 3.12a2.499 2.499 0 0 1 0 1.504l6.718 3.12a2.5 2.5 0 1 1-.488.876l-6.718-3.12a2.5 2.5 0 1 1 0-3.256l6.718-3.12A2.5 2.5 0 0 1 11 2.5zm-8.5 4a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3zm11 5.5a1.5 1.5 0 1 0 0 3 1.5 1.5 0 0 0 0-3z" />
+              </svg>
+              Share
+            </button>
           </div>
         </div>
 
@@ -243,6 +261,46 @@ const TextEditor = () => {
 
       {/* Quill editor container */}
       <div className="editor-container" ref={editorRef}></div>
+
+      <div id="shareModal" className="modal-you modal-overlay">
+        <div className="modal-you modal-content">
+          <div className="modal-you modal-header">
+            <h3>Share document</h3>
+            <button
+              className="modal-you close-button"
+              onClick={() =>
+                (document.getElementById("shareModal").style.display = "none")
+              }
+            >
+              &times;
+            </button>
+          </div>
+          <div className="modal-you modal-body">
+            <p>
+              To collaborate in real-time with others, share this link with
+              them. Once they log in, you'll be able to see their changes as
+              they happen.
+            </p>
+            <div className="modal-you share-link-container">
+              <input
+                type="text"
+                className="modal-you share-link"
+                value={window.location.href}
+                readOnly
+              />
+              <button
+                className="modal-you copy-button"
+                onClick={() => {
+                  navigator.clipboard.writeText(window.location.href);
+                  alert("Link copied to clipboard!");
+                }}
+              >
+                Copy
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
